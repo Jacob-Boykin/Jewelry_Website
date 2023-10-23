009ffef5ee44ab1d44644eb2ed2ce60e37a0909a
jmboykin <jmboykin@crimson.ua.edu>
2023-10-23 11:55:24 -0500

    Created the project in visual studio
    Created the project but haven't added anything to it. Assignment 4 only requires us to create the repo, the project, push the code, and submit

---
+++ Jewely_Energie/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Jewely_Energie.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
